67cd1dd7a5e810f04d24e8e916d45a36e1b9f3f5
demidraksin <greatraksin@icloud.com>
2022-01-19 10:23:37 +0300

    Adding main page 🏠

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__)  # сам сайт
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/')  # говорит, что будет происходить, если перейти на главную страницу сайта
+def index():  # функция, которая будет отображать главную страницу сайта
+    title = 'Главная страница'  # задаю заголовок странице
+    return render_template('index.html', title=title)  # отрисовать шаблон главной страницы
 
 
 if __name__ == '__main__':

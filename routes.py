0247f488cdf2d6a0277b5f34c83c23edb2d95839
demidraksin <greatraksin@icloud.com>
2022-02-02 10:02:25 +0300

    Adding routes 🧠

--- routes.py
+++ routes.py
@@ -17,3 +17,34 @@ def contacts():
 @app.route('/about')  # новая страница о нас
 def about_us():
     return render_template('about.html')
+
+
+@app.route('/catalog')
+def catalog():
+    goods = [
+        {
+            'good_name': 'Молоток "Тор"',
+            'good_price': 89.99,
+            'good_description': 'Лучший молоток на рынке молотков.',
+            'good_image': 'https://cdn-icons-png.flaticon.com/512/2505/2505252.png',
+        },
+        {
+            'good_name': 'Пила "Дружба"',
+            'good_price': 129.45,
+            'good_description': 'Распили своего друга весело.',
+            'good_image': 'https://cdn-icons-png.flaticon.com/512/3428/3428061.png',
+        },
+        {
+            'good_name': 'Перфоратор "Привет, сосед!"',
+            'good_price': 899.34,
+            'good_description': 'Вашему соседу скучно жить? Подарите ему радость.',
+            'good_image': 'https://cdn-icons.flaticon.com/png/512/3108/premium/3108914.png?token=exp=1643786118~hmac=df37d4cba5b8da32fe6538193aae45b7',
+        },
+        {
+            'good_name': 'Пассатижи "Первый поцелуй"',
+            'good_price': 67.19,
+            'good_description': 'Каждый раз как в первый раз. Больно и странно.',
+            'good_image': 'https://cdn-icons-png.flaticon.com/512/971/971912.png',
+        },
+    ]
+    return render_template('catalog.html')
